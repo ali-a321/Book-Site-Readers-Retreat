2bbd1c548b41270118fbb85ffcd0008de7a1c461
ali-a321 <111758255+ali-a321@users.noreply.github.com>
2023-06-13 12:53:44 -0400

    Add files via upload

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -27,40 +27,40 @@ const createUser = async (req, res) => {
   };
 
 //LOGIN, POST, /users/login
-
 const loginUser = async (req, res) => {
-    try {
-      const { username, password } = req.body;
-  
-      // Check if the user exists in the database
-      const query = 'SELECT * FROM users WHERE username = ?';
-      db.query(query, [username], async (error, results) => {
-        if (error) {
-          console.error('Error logging in:', error);
-          return res.status(500).json({ message: 'Failed to log in' });
-        }
-  
-        if (results.length === 0) {
-          return res.status(401).json({ message: 'Invalid credentials' });
-        }
-  
-        // Verify the password
-        const user = results[0];
-        const isPasswordValid = await bcrypt.compare(password, user.password);
-        if (!isPasswordValid) {
-          return res.status(401).json({ message: 'Invalid credentials' });
-        }
-  
-        // Generate JWT token
-        const token = jwt.sign({ userId: user.id }, process.env.JWTSECRET, { expiresIn: '1h' });
-  
-        return res.status(200).json({ token });
-      });
-    } catch (error) {
-      console.error('Error logging in:', error);
-      res.status(500).json({ message: 'Failed to log in' });
-    }
-  };
+  try {
+    const { username, password } = req.body;
+
+    // Check if the user exists in the database
+    const query = 'SELECT * FROM users WHERE username = ?';
+    db.query(query, [username], async (error, results) => {
+      if (error) {
+        console.error('Error logging in:', error);
+        return res.status(500).json({ message: 'Failed to log in' });
+      }
+
+      if (results.length === 0) {
+        return res.status(401).json({ message: 'Username does not exist' });
+      }
+
+      // Verify the password
+      const user = results[0];
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+      if (!isPasswordValid) {
+        return res.status(401).json({ message: 'Incorrect password' });
+      }
+
+      // Generate JWT token
+      const token = jwt.sign({ userId: user.id }, process.env.JWTSECRET, { expiresIn: '1h' });
+
+      return res.status(200).json({ token });
+    });
+  } catch (error) {
+    console.error('Error logging in:', error);
+    res.status(500).json({ message: 'Failed to log in' });
+  }
+};
+
   //GET, /user
   const getUser = (req, res) => {
     const { username, id } = req;
